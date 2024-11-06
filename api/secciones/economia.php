d3bbe555ba5b48aa98747df63c5d1a998f4fa663
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:43:58 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $economia = [
 "titulo" => "LA ECONOMIA",
-"autor" => "Anthony Chávez",
+"autor" => "EconomíaTV",
 "resumen" => "La economía se divide en dos ramas principales:
 Economía positiva: Describe y explica fenómenos económicos.
 Economía normativa: Se enfoca en juicios sobre cómo deberían ser los sistemas económicos. 
