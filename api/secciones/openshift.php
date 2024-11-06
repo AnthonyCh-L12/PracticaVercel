b7620f26b68b4f408878d8a542b70c8236304a6d
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:33:37 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat OpenShift",
+"autor" => "Anthony Chávez",
+"resumen" => "OpenShift, formalmente llamado Openshift Container Platform, es un producto de computación en la nube de plataforma como servicio de Red Hat.",
 ];
 ?>
