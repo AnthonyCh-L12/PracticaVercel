527a28e6010848bd173cbde91fa0373035b88fcc
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:34:11 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "OpenStack: Open Source Cloud Computing Infrastructure",
+"autor" => "Anthony Chávez",
+"resumen" => "OpenStack es un proyecto de computación en la nube para proporcionar una infraestructura como servicio. Es un software libre y de código abierto distribuido bajo los términos de la licencia Apache.",
 ];
 ?>
