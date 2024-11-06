162d99fb964b3adb800142e4dba64f59c2cb5e1e
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:45:00 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $internacional = [
 "titulo" => "DONALD TRUMP PRESIDENCIA",
-"autor" => "Anthony Chávez",
+"autor" => "UNS National",
 "resumen" => "Las elecciones presidenciales de 2024 se celebrarán el martes 5 de noviembre de 2024, y serán las sexagésimas elecciones presidenciales en Estados Unidos.​ Los votantes eligen a los compromisarios que a su vez deberán escoger al nuevo presidente y vicepresidente a través del Colegio Electoral.",
 ];
 ?>
