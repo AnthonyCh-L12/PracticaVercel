fb85056766f5792021175b07138a181a4416dbda
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:29:31 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "GENTE CHIMBOTE",
+"autor" => "Anthony Chávez",
+"resumen" => "Disparando fusiles automáticos, utilizando artillería de mortero, atravesando un río, desplazándose por cuerdas suspendidas, de rodillas y completando circuitos complicados.",
 ];
 ?>
