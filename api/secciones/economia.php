c5316c09647e7f84876f0ec2703e422ecb46bdaf
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:27:17 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "LA ECONOMIA",
+"autor" => "Anthony Chávez",
+"resumen" => "La economía dentro del perú está decayendo cada día más",
 ];
 ?>
