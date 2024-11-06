efafc5264b07d40a02bcdb4e5c92962af36ad90c
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:43:23 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $contenedores = [
 "titulo" => "CONTENEDORES",
-"autor" => "Anthony Chavez",
+"autor" => "iContainers",
 "resumen" => "La estandarización de las dimensiones de los contenedores tuvo un impacto masivo en el comercio mundial. Los fabricantes podían ofrecer en todo el globo una inimaginable variedad de productos a bajo coste, cargando las mercancías en sus fábricas y enviándolas directamente a tiendas y almacenes de todo el mundo.",
 ];
 ?>
