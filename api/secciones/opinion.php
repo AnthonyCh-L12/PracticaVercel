e98f1539fb927cc2efbfa1da385ca7272a6cfc69
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:34:50 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La opinión pública y la democracia peruana en la mirada del Barómetro de las Américas",
+"autor" => "Anthony Chávez",
+"resumen" => "Estas actitudes se analizan utilizando los datos de las encuestas del Barómetro de las Américas. Los hallazgos más importantes son dos. El primero es que el apoyo a la idea de la democracia es bajo en el Perú en comparación con el promedio regional y este apoyo ha caído un tanto desde el año 2012. De igual manera, el rechazo ciudadano a acciones antidemocráticas como golpes militares y el cierre del Congreso a manos del presidente es relativamente bajo en términos comparativos con otros países del continente.",
 ];
 ?>
