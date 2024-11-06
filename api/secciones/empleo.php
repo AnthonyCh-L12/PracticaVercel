8c96b3d3f79b30ba3122f352e92e35d593ed032a
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:27:54 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Empleabilidad - Noviembre",
+"autor" => "Anthony Chávez",
+"resumen" => "Empleos por turnos, turno día, noche y jornada completa",
 ];
 ?>
