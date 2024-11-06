bf6b1c523c67fd3bccbafb9e681de86afd22b8ee
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:44:45 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $git = [
 "titulo" => "GitHub",
-"autor" => "Anthony Chávez",
+"autor" => "GitHub",
 "resumen" => "GitHub es una forja para alojar proyectos utilizando el sistema de control de versiones Git. Se utiliza principalmente para la creación de código fuente de programas de ordenador.",
 ];
 ?>
