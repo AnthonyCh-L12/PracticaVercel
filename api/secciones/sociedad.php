8025316b82700dd9544fee9d8c98eece776f1cba
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:35:54 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Policía terna cae en asalto",
+"autor" => "Anthony Chávez",
+"resumen" => "Efectivo policial cae en asalto con un grupo de delincuentes que intentaban robar a una tienda en el A.H. 3 estrellas",
 ];
 ?>
