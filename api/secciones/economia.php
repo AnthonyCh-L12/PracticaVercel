04dfefab95988ad47fb9ea1fb709ac91e6ec653e
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:40:03 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -6,6 +6,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $economia = [
 "titulo" => "LA ECONOMIA",
 "autor" => "Anthony Chávez",
-"resumen" => "La economía dentro del perú está decayendo cada día más",
+"resumen" => "La economía se divide en dos ramas principales:
+Economía positiva: Describe y explica fenómenos económicos.
+Economía normativa: Se enfoca en juicios sobre cómo deberían ser los sistemas económicos. 
+La microeconomía se refiere a las elecciones de los agentes económicos para satisfacer sus necesidades y la escasez. La macroeconomía analiza la economía como un sistema nacional, global o internacional. ",
 ];
 ?>
