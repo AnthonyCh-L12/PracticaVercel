f691936e1af2b7d7a8d19e969ae76b5ca7251d9d
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:32:39 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "DONALD TRUMP PRESIDENCIA",
+"autor" => "Anthony Chávez",
+"resumen" => "Donald Trump gana las elecciones presidenciales de EE.UU., proyecta CNN",
 ];
 ?>
