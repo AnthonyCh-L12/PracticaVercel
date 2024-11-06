45a946baa8523059df119f0830f8f38fdd8f0c46
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:39:09 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $cultura = [
 "titulo" => "LA CULTURA",
 "autor" => "Anthony Chávez",
-"resumen" => "La cultura maya dentro de la actualidad",
+"resumen" => "Algunos etólogos han hablado de «cultura» para referirse a costumbres, actividades o comportamientos transmitidos de una generación a otra en grupos de animales por imitación consciente de dichos comportamientos.[cita requerida] Las creencias y prácticas de una cultura determinada pueden ser ejercidas como mecanismos de control que limitan la conducta social.",
 ];
 ?>
