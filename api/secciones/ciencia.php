8a89a5cf7fed74ac0682e365a626c07d0079324b
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:41:20 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "EL AMBIENTE",
+"titulo" => "CAMBIO CLIMÁTICO",
 "autor" => "Anthony Chavez",
-"resumen" => "El ambiente de hoy en día.",
+"resumen" => "La preocupación por el medio ambiente es cada día mayor. En las últimas décadas, el ámbito científico viene detectando cambios profundos que suponen un impacto ambiental en nuestro planeta. Entre otros, el aumento de la temperatura global, el deshielo en los polos o la subida del nivel del mar, ponen en jaque la vida tal como la conocemos. La contaminación, especialmente la emisión de gases de efecto invernadero por la acción humana contribuye crucialmente a ese deterioro.",
 ];
 ?>
