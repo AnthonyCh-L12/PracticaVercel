555a036985b90399e93035a41633af51528e1ee1
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:42:19 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -6,9 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $contenedores = [
 "titulo" => "CONTENEDORES",
 "autor" => "Anthony Chavez",
-"resumen" => "Una década después, con la ayuda de la armada americana, la estandarización de los contenedores llevó al desarrollo de una norma internacional llamada ISO (International Standards Organization) shipping container.
-
-
-La estandarización de las dimensiones de los contenedores tuvo un impacto masivo en el comercio mundial. Los fabricantes podían ofrecer en todo el globo una inimaginable variedad de productos a bajo coste, cargando las mercancías en sus fábricas y enviándolas directamente a tiendas y almacenes de todo el mundo.",
+"resumen" => "La estandarización de las dimensiones de los contenedores tuvo un impacto masivo en el comercio mundial. Los fabricantes podían ofrecer en todo el globo una inimaginable variedad de productos a bajo coste, cargando las mercancías en sus fábricas y enviándolas directamente a tiendas y almacenes de todo el mundo.",
 ];
 ?>
