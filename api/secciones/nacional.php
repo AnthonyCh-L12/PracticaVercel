15576ac9e138356543352046777b99e8d1363212
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:45:12 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -5,7 +5,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 
 $nacional = [
 "titulo" => "Perú Libre rechaza acuerdo para que policías y militares sean juzgados en otro fuero",
-"autor" => "Anthony Chávez",
+"autor" => "TV Perú",
 "resumen" => "Flavio Cruz cuestiona el papel de las autoridades del Ministerio Público y el Poder Judicial por no oponerse a la medida en reunión del Consejo de Estado",
 ];
 ?>
