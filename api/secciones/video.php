c7093fbee50c44fdefd5eb432ac5a19f8f4d4e43
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:37:58 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Vídeos de Tendencia",
+"autor" => "Anthony Chávez",
+"resumen" => "Lo más popular de YouTube. No te pierdas los últimos vídeos musicales, tráileres, vídeos graciosos y todo lo que la gente está viendo en estos momentos.",
 ];
 ?>
